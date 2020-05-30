b83f846b1b72ccc0504b51dba5a60e31d2e27a54
watanaberyosuke <ryoosuke1992@gmail.com>
2020-05-31 01:30:17 +0900

    sessionテスト

---
+++ spec/requests/users_login_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Sessions", type: :request do
+  let!(:user) { create(:user) }
+
+  it "正常なレスポンスを返すこと" do
+    get login_path
+    expect(response).to be_success
+    expect(response).to have_http_status "200"
+  end
+
+end
