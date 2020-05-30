82def66795adc6ff1f0dde40b85ccefc8c05e188
watanaberyosuke <ryoosuke1992@gmail.com>
2020-05-30 20:34:57 +0900

    userコントローラーにアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,25 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "旅Lifeへようこそ！"
+      redirect_to @user
+    else
+      render ''new
+    end
   end
 
   def index
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
