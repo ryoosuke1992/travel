df746dd2eb87e1dd369682c03411d2ddc8413940
watanaberyosuke <ryoosuke1992@gmail.com>
2020-06-04 10:47:38 +0900

    edit updateアクションの追加

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -6,13 +6,31 @@ class PlansController < ApplicationController
 		@plan = Plan.new
 	end
 
+	def show
+		@plan = Plan.find(params[:id])
+	end
+
 	def create
 		@plan = current_user.plans.new(plan_params)
 		if @plan.save
-			flash[:success] = "新規旅行が投稿されました"
-			redirect_to root_path
+			flash[:success] = "新規旅行)が投稿されました"
+			redirect_to @plan
 		else
 			render 'plans/new'
+		end
+	end
+
+	def edit
+		@plan = Plan.find(params[:id])
+	end
+
+	def update
+		@plan = Plan.find(params[:id])
+		if @plan.update_attributes(plan_params)
+			flash[:success] = "旅行情報が更新されました"
+			redirect_to @plan
+		else
+			render 'edit'
 		end
 	end
 	
