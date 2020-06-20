36d6be4a88f1434022411db269c22ce3af1719d3
ryoosuke1992 <nabesuke1992@gmail.com>
2020-06-20 23:45:10 +0900

    participantモデルの作成

---
+++ db/migrate/20200620143406_create_participants.rb
@@ -0,0 +1,9 @@
+class CreateParticipants < ActiveRecord::Migration[5.2]
+  def change
+    create_table :participants do |t|
+      t.references :user, foreign_key: true
+      t.references :plan, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_140202) do
+ActiveRecord::Schema.define(version: 2020_06_20_143406) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "plan_id"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 2020_06_10_140202) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_notifications_on_user_id"
+  end
+
+  create_table "participants", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "plan_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["plan_id"], name: "index_participants_on_plan_id"
+    t.index ["user_id"], name: "index_participants_on_user_id"
   end
 
   create_table "plans", force: :cascade do |t|
