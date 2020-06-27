b793a3b007160280fa2ed189d5a95750e20bcc44
ryoosuke1992 <nabesuke1992@gmail.com>
2020-06-27 18:16:24 +0900

    通知のページネーション追加

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :logged_in_user
 
   def index
-    @notifications = current_user.notifications.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
+    @notifications = current_user.notifications.order(created_at: :desc).paginate(page: params[:page], per_page: 4)
     current_user.update_attribute(:notification, false)
   end
 end
